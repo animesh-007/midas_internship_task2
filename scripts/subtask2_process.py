18f75a9a61e8d1b01989c3cf0e5a321b4d257476
Animesh Gupta <animeshagarwal7@gmail.com>
2021-04-10 23:15:41 +0400

    Update subtask2_process.py

--- scripts/subtask2_process.py
+++ scripts/subtask2_process.py
@@ -6,30 +6,30 @@ from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='Split Midas task 2 dataset')
 parser.add_argument(
-        "--path",
+        "--subtask2",
         nargs="?",
         type=str,
-        default="./subtask2",
+        default="../subtask2/train",
         help="Path to store the dataset",
     )
 parser.add_argument(
         "--subtask1",
         nargs="?",
         type=str,
-        default="./subtask1",
+        default="../subtask1",
         help="Path to the subtask1 dataset",
     )
 args = parser.parse_args()
 
-folders = (natsorted(glob(args.subtask1+"train/*")))
+folders = (natsorted(glob(args.subtask1+"/train/*")))
 
 folders = folders[0:10]
 
-os.makedirs("../subtask2/train",exist_ok=True)
+os.makedirs(args.subtask2,exist_ok=True)
 
 for folder in folders:
     print(folder)
-    train_destination = f"../subtask2/train/{os.path.split(folder)[-1]}"
+    train_destination = f"{args.subtask2}/{os.path.split(folder)[-1]}"
     shutil.copytree(folder,train_destination)
 
 
