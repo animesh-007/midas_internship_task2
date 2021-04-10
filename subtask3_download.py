4a37919791669e7e2b8a1d5a98d1ebc7cae29b7a
Animesh Gupta <animeshagarwal7@gmail.com>
2021-04-10 23:28:18 +0400

    Update subtask3_download.py

--- subtask3_download.py
+++ subtask3_download.py
@@ -9,7 +9,7 @@ parser.add_argument(
         "--path",
         nargs="?",
         type=str,
-        default="../subtask3",
+        default="./subtask3_data",
         help="Path to store the dataset",
     )
 args = parser.parse_args()
