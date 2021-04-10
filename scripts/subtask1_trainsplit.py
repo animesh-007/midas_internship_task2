e7608e842e275f07b62493d6409c02ab0aaff73b
Animesh Gupta <animeshagarwal7@gmail.com>
2021-04-10 23:11:56 +0400

    Update and rename split_dataset.py to subtask1_trainsplit.py

--- scripts/subtask1_trainsplit.py
+++ scripts/subtask1_trainsplit.py
@@ -1,3 +1,4 @@
+#author: animesh-007
 import os
 from glob import glob
 from tqdm import tqdm
@@ -6,24 +7,23 @@ import argparse
 import random
 random.seed(230)
 
+## arguments used while running this file
 parser = argparse.ArgumentParser(description='Split Midas task 1 dataset')
 parser.add_argument(
         "--path",
         nargs="?",
         type=str,
-        default="./subtask1",
+        default="../subtask1",
         help="Path to the dataset and splitting in same folder",
     )
 args = parser.parse_args()
 path = args.path + "/train/*"
 
-
+## read data inside the folder
 for folder in tqdm(glob(path)):
     files = glob(folder+"/*")
     files.sort() # make sure that the filenames have a fixed order before shuffling
     random.shuffle(files) # shuffles the ordering of filenames (deterministic given the chosen seed)
-
-    
     split_1 = int(0.8 * len(files))
     train_filenames = files[:split_1]
     val_filenames = files[split_1:]
